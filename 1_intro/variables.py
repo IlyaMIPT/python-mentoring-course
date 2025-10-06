fe7111099419785e60fe251b18c2f00634a16ccd
Илья Малюга <vbd1988@yandex.ru>
2025-10-06 10:21:28 +0300

    Create variables examples

---
+++ 1_intro/variables.py
@@ -0,0 +1,9 @@
+
+
+# создаем переменные-строки
+first_name = "Александр"
+middle_name = "Сергеевич"
+last_name = "Пушкин"
+poet_name = first_name + " " + middle_name + " " + last_name
+print("Любимый поэт")
+print(poet_name)
---
+++ 2_variables/2_floats.py
@@ -0,0 +1,6 @@
+# создаем переменные, дроби
+rub_price = 1000
+usd_exchange_rate = 97.3
+usd_price = rub_price / usd_exchange_rate
+print('Цены в долларах и рублях')
+print(rub_price, usd_price)
