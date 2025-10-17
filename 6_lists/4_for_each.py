e7c81519a0c08d9c5d6ed2c3194e47c4d292951c
Ilya Malyuga <vbd1988@yandex.ru>
2025-10-17 09:10:42 +0300

    For each materials

--- 6_lists/4_for_each.py
+++ 6_lists/4_for_each.py
@@ -4,5 +4,11 @@ reasons = [
     "I like coding",
     "I want to start career in data science"
 ]
+print("All my reasons to start learning Python")
 for reason in reasons:
     print(f"I like python because of {reasons}")
+
+# тоже самое, но с помощью range:
+print("Display all list members using range function")
+for i in range(len(reasons)):
+    print(reasons[i])
