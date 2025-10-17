b9845d0c3574d81f938030c68ce1e360d51cf722
Ilya Malyuga <vbd1988@yandex.ru>
2025-10-17 09:39:56 +0300

    Create slices example

---
+++ 6_lists/6_slices.py
@@ -0,0 +1,17 @@
+reasons = [
+    "start a career",
+    "become rich",
+    "do something great",
+    "develop great products",
+    "passion to learn new things",
+    "desire to contribute to opensource"
+]
+
+print("What you like:")
+print(reasons[:2])
+
+print("What corporations like:")
+print(reasons[2:4])
+
+print("What professionals like:")
+print(reasons[4:])
