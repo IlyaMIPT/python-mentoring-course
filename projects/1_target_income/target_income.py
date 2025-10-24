1f35c807ddcec6228c03817583720df840e95c3b
Илья Малюга <vbd1988@yandex.ru>
2025-10-24 14:10:22 +0300

    Minor changes

--- projects/1_target_income/target_income.py
+++ projects/1_target_income/target_income.py
@@ -1,14 +1,15 @@
 TARGET_SALARY = 1000000
 SALARY_GROWTH = 30
 STARTER_SALARY = 300000
-PERIOD_LEN_YEARS = 4
-SPENDING_SHARE = 20
+PERIOD_LEN_YEARS = 5
+SPENDING_SHARE = 10
 DEPOSIT_INTEREST_RATE = 17
 
 deposits_balance = 0
 current_salary = STARTER_SALARY
 is_target_income_reached = False
 for month in range(0, PERIOD_LEN_YEARS * 12):
+    # блок роста зарплаты
     if month % 12 == 0 and month > 0:
         salary_growth = round(current_salary * SALARY_GROWTH / 100)
         current_salary += salary_growth
@@ -28,3 +29,4 @@ if not is_target_income_reached:
     print("Целевой доход, к сожалению, не достигнут")
     print('Фактический общий доход', total_months_income)
     print('В том числе зарплата', current_salary, 'и проценты', interest_income)
+    print('На депозитах:', deposits_balance)
