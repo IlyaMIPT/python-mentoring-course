d87e5e852cce3d31515504529d5b63e9a123eace
Илья Малюга <vbd1988@yandex.ru>
2025-11-06 07:33:59 +0300

    Some edits

--- 10_tuples/1_tuples_intro.py
+++ 10_tuples/1_tuples_intro.py
@@ -8,3 +8,11 @@ print(ivan_senior)
 # обратимся к элементам кортежа
 print('Имя:', ivan_senior[0], 'должность:',
       ivan_senior[1], 'зарплата:', ivan_senior[2])
+
+# обойти элементы кортежа в цикле
+for attr in ivan_senior:
+    print(attr)
+
+# распаковка элементов кортежа
+person, grade, salary = ivan_senior
+print(person, grade, salary)
