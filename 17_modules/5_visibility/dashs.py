ac372e41663b905ab266f118d0eb531c511bbcff
Илья Малюга <vbd1988@yandex.ru>
2026-01-14 09:01:11 +0300

    add visibility example

--- 17_modules/5_visibility/dashs.py
+++ 17_modules/5_visibility/dashs.py
@@ -4,6 +4,17 @@ dashboards = [
     "suppliers"
 ]
 
+last_dash_id = 12345
+
 
 def add_dashboard(d: str) -> None:
     dashboards.append(d)
+
+
+def inc_version(delta: int) -> None:
+    global last_dash_id
+    last_dash_id += delta
+
+
+def get_version() -> int:
+    return last_dash_id
---
+++ 17_modules/5_visibility/import_name.py
@@ -0,0 +1,12 @@
+from dashs import (
+    last_dash_id,
+    inc_version,
+    get_version
+)
+
+last_dash_id = 7
+
+inc_version(1)
+
+print(last_dash_id)
+print(get_version())
