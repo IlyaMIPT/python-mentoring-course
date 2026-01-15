3dd5ed4d801a240825af1fe4b739ff720f3b3038
Ilya Malyuga <vbd1988@yandex.ru>
2026-01-15 17:33:03 +0300

    Minor changes

--- 17_visibility/3_legb.py
+++ 17_visibility/3_legb.py
@@ -3,6 +3,11 @@
 DEFAULT_FROM = 0
 
 
+def set_default_from(new_value: int) -> None:
+    global DEFAULT_FROM
+    DEFAULT_FROM = new_value
+
+
 def get_range(max: int) -> list[int]:
     # get_range в глобальной области видимости
     # max - локальная, переопределили встроенную функцию
